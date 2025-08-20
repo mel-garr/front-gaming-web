fda649597d2abcecf0fdfd7dc2611782a89d3a75
++ <kido9odik@gmail.com>
2025-08-20 07:19:38 +0100

    bs

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// check if any data alredy exist
+function check_save(){
+    if (localStorage.getItem("playerscore") != null)
+        return (1);
+    else if (sessionStorage.getItem("playerscore") != null)
+        return(2);
+    else
+        return (3);
+}
+
+// store if local
+function update_in_local(key){
+    let current = parseInt(localStorage.getItem(key)) || 0;
+    localStorage.setItem(key, current + 1);
+}
+
+function init_in_local(){
+    if (sessionStorage.getItem("playerscore") != null || sessionStorage.getItem("computerscore") != null){
+        sessionStorage.removeItem("playerscore");
+        sessionStorage.removeItem("computerscore");
+    }
+    localStorage.setItem("playerscore", 0);
+    localStorage.setItem("computerscore", 0);
+}
+
+// store if session
+function update_in_session(key){
+    let current = parseInt(sessionStorage.getItem(key)) || 0;
+    sessionStorage.setItem(key, current + 1);
+}
+
+function init_in_session(){
+    sessionStorage.setItem("playerscore", 0);
+    sessionStorage.setItem("computerscore", 0);
+}
